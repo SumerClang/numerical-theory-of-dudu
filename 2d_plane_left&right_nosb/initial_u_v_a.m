272551572ab472b8f1fee2cae3d7662ed445bb15
Sumer Clang <124630145+SumerClang@users.noreply.github.com>
2024-05-11 09:21:30 +0800

    Delete 2d_plane_left&right_nosb directory

--- 2d_plane_left&right_nosb/initial_u_v_a.m
+++
@@ -1,8 +0,0 @@
-function [displacement,velocity,acceleration] = initial_u_v_a(number_of_point,dimen)
-%INITIAL_U_V_A 根据具体情况对位移，速度，加速度进行初始化
-displacement=zeros(number_of_point,dimen);
-velocity=zeros(number_of_point,dimen);
-acceleration=zeros(number_of_point,dimen);
-fprintf("已成功初始化u,v,a\n");
-end
-
